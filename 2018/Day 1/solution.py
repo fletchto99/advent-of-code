02df4132078087d15ce54d4db4b30205e4aba357
Matt Langlois <fletchto99@github.com>
2018-12-01 10:57:12 -0700

    Day 1

---
+++ 2018/Day 1/solution.py
@@ -0,0 +1,21 @@
+with open('input.txt') as fp:
+    lines = fp.readlines()
+    frequencies = {0: True}
+    frequency = 0
+    repeated = None
+    printed = False
+
+    while repeated is None:
+      for line in lines:
+        frequency += int(line)
+        if frequency in frequencies and repeated is None:
+          repeated = frequency
+        frequencies[frequency] = True
+
+      if printed is False:
+        print(frequency)
+        printed = True
+
+    print(repeated)
+
+
