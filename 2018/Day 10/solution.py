94806555ce862c72f16fea9bb7a410d003c5c5ab
Matt Langlois <fletchto99@github.com>
2018-12-10 15:02:46 -0700

    Final solution

--- 2018/Day 10/solution.py
+++ 2018/Day 10/solution.py
@@ -1,4 +1,5 @@
 points = []
+potentials = []
 size = 350
 f = open("output.txt", "w")
 
@@ -9,7 +10,6 @@ for line in open('input.txt'):
     'x_velocity': int(line.split("<")[2].split(",")[0].strip()),
     'y_velocity': int(line.split("<")[2].split(",")[1].split(">")[0].strip())
   })
-
 
 for i in range(15000):
   text = [['.' for _ in range(size)] for _ in range(size)]
@@ -22,12 +22,28 @@ for i in range(15000):
     else:
       skip = True
   if not skip:
-    f.write("Solved in: " + str(i) + "\n")
-    for i in range(len(text)):
-      if len(set(text[i])) > 1:
-        f.write(''.join(text[i]) + "\n")
-    f.write('\n\n\n\n\n\n\n\n\n')
+    lines = []
+    count = 0
+    trim_start = size
+    trim_end = 0
+    for x in range(len(text)):
+      if len(set(text[x])) > 1:
+        count += 1
+        lines.append(''.join(text[x]))
+        if text[x].index("#") < trim_start:
+          trim_start = text[x].index("#")
+        last_index = len(text[x]) - text[x][::-1].index("#") - 1
+        if last_index > trim_end:
+          trim_end = last_index
+    potentials.append({
+      'msg': '\n'.join([line[trim_start:trim_end] for line in lines]),
+      'count': count,
+      'time': i
+    })
+  if i % 100 == 0:
+    print(str(i) + "/15000")
 
+potentials = sorted(potentials, key=lambda k: k['count'])
 
-
-
+print(potentials[0]['msg'])
+print('Found at: ' + str(potentials[0]['time']))
